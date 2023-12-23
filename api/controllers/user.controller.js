e9c9dc604ad147603aa629b794ee9106b9b8fb52
Usama <fa18-bcs-083@cuilahore.edu.pk>
2023-12-23 13:45:14 +0500

    create sign up api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,13 @@
-export const test = (req,res)=>{
-    
-        res.json({
-          message: "Hello World",
-        });
-      };
+import User from "../models/user.model.js";
+
+export const test = async (req, res) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
+  await newUser.save();
+  try {
+    res.status(201).json("user created successfully");
+  } catch {
+    res.status(500).json(error.message);
+  }
+};
